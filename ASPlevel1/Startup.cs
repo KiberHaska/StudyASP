eeaafcadd4ffb7ce597eb1e5119a26040752c01c
Astrid <email@email.local>
2020-10-27 16:36:08 +0300

    Lesson1: Work Configuration

--- ASPlevel1/Startup.cs
+++ ASPlevel1/Startup.cs
@@ -7,18 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace ASPlevel1
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,13 +30,16 @@ namespace ASPlevel1
                 app.UseDeveloperExceptionPage();
             }
 
+            var hello = _configuration["CustomHelloWorld"];
+            //var logLevel = _configuration["Logging:LogLevel:Default"];
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(hello);
                 });
             });
         }
