a672847ba20fe00388121ed05a91181427a9c642
Astrid <email@email.local>
2020-10-28 18:36:13 +0300

    Lesson1: HomeWork! architecture editing project

---
+++ ASPlevel1/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPlevel1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPlevel1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly List<EmployeeViewModel> _employees = new List<EmployeeViewModel>
+        {
+            new EmployeeViewModel
+            {
+                Id = 1,
+                FirstName = "Ivan",
+                SurName = "IvanOff",
+                Patronymic = "Ivanovich",
+                Age = 22,
+                Position = "Boss"
+            },
+            new EmployeeViewModel
+            {
+                Id = 2,
+                FirstName = "Vlad",
+                SurName = "PetrOff",
+                Patronymic = "Ivanovich",
+                Age = 35,
+                Position = "Programmer"
+            }
+            };
+       
+        public IActionResult Employees()
+        {           
+            return View(_employees);
+        }
+
+        public IActionResult EmployeeDetails(int id)
+        {
+            var employee = _employees.FirstOrDefault(x => x.Id == id);
+            if (employee == null)
+                return NotFound(); //return 404 Not Found
+
+            return View(employee);
+        }
+    }
+}
---
+++ ASPlevel1/Controllers/OfficeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPlevel1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPlevel1.Controllers
+{
+    public class OfficeController : Controller
+    {
+        private readonly List<OfficeViewModel> _employees = new List<OfficeViewModel>
+        {
+            new OfficeViewModel
+            {
+                Id = 1,
+                Name = "Office1",
+                Address = "1 Street"                
+            },
+            new OfficeViewModel
+            {
+                Id = 2,
+                Name = "Office2",
+                Address = "2 Street"                
+            },
+            new OfficeViewModel
+            {
+                Id = 3,
+                Name = "Office3",
+                Address = "3 Street"
+            }
+            };
+        
+        public IActionResult Offices()
+        {           
+            return View(_employees);
+        }
+
+        public IActionResult OfficeDetails(int id)
+        {
+            var office = _employees.FirstOrDefault(x => x.Id == id);
+            if (office == null)
+                return NotFound(); //return 404 Not Found
+
+            return View(office);
+        }
+    }
+}
