f97f609435b33af3f18670f81531b4c5aca6d483
Astrid <email@email.local>
2020-10-30 11:47:59 +0300

    Lesson2: Edit files, before rendering layouts

--- ASPlevel1/Controllers/HomeController.cs
+++ ASPlevel1/Controllers/HomeController.cs
@@ -8,45 +8,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace ASPlevel1.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly List<EmployeeViewModel> _employees = new List<EmployeeViewModel>
-        {
-            new EmployeeViewModel
-            {
-                Id = 1,
-                FirstName = "Ivan",
-                SurName = "IvanOff",
-                Patronymic = "Ivanovich",
-                Age = 22,
-                Position = "Boss"
-            },
-            new EmployeeViewModel
-            {
-                Id = 2,
-                FirstName = "Vlad",
-                SurName = "PetrOff",
-                Patronymic = "Ivanovich",
-                Age = 35,
-                Position = "Programmer"
-            }
-            };
+    {       
         public IActionResult Index()
-        {
-            //return Content("Hello from controller!");
+        {            
             return View();
-        }
-        public IActionResult Employees()
-        {           
-            return View(_employees);
-        }
-
-        public IActionResult EmployeeDetails(int id)
-        {
-            var employee = _employees.FirstOrDefault(x => x.Id == id);
-            if (employee == null)
-                return NotFound(); //return 404 Not Found
-
-            return View(employee);
-        }
+        }       
     }
 }
