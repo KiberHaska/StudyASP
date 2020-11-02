3df609afd6ebfd3bc3860a3b66fb7aa6b1bfa3d6
Astrid <email@email.local>
2020-11-02 18:45:08 +0300

    Lesson3: Edit Startup app.Use app.Run app.UseMiddleware (TokenMiddleware)

---
+++ ASPlevel1/Infrastructure/TokenMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPlevel1.Infrastructure
+{
+    public class TokenMiddleware
+    {
+        private const string correctToken= "123qew";
+        public RequestDelegate Next { get; }
+        public TokenMiddleware (RequestDelegate next)
+        {
+            Next = next;
+        }
+        public async Task InvokeAsync (HttpContext context)
+        {
+            var token = context.Request.Query["referenceToken"];
+
+            if (string.IsNullOrEmpty(token))
+            {
+                await Next.Invoke(context);
+                return;
+            }
+            if (token == correctToken)
+            {
+                // work
+                await Next.Invoke(context);
+            }
+            else
+            {
+                await context.Response.WriteAsync("Token is incorrect");
+            }
+        }
+    }
+}
--- ASPlevel1/Startup.cs
+++ ASPlevel1/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using ASPlevel1.Infrastructure;
 
 namespace ASPlevel1
 {
@@ -34,6 +35,11 @@ namespace ASPlevel1
 
             //var hello = _configuration["CustomHelloWorld"];
             //var logLevel = _configuration["Logging:LogLevel:Default"];
+            app.Map("/index", CustomIndexHandler);
+
+            UseMiddlewareSample(app);
+
+            app.UseMiddleware<TokenMiddleware>();           
 
             app.UseRouting();
 
@@ -47,6 +53,36 @@ namespace ASPlevel1
                 //{
                 //    await context.Response.WriteAsync(hello);
                 //});
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello! app.run test, The End! ");
+            });
+        }
+
+        private void UseMiddlewareSample(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                bool isError = false;
+                //...
+                if (isError)
+                {
+                    await context.Response.WriteAsync("Error occured.");
+                }
+                else
+                {
+                    await next.Invoke();
+                }
+            });
+        }
+
+        private void CustomIndexHandler(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello! Custom Index ");
             });
         }
     }
