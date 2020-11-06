ca0330dc635d0e5ec22d0bec191d80a645f8fdbe
Astrid <email@email.local>
2020-11-06 18:02:10 +0300

    Lesson4: Categories Add ViewComponent

---
+++ ASPlevel1/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AspLevel1.Domain.Entities.Base;
+using AspLevel1.Domain.Entities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPlevel1.ViewModels
+{
+    public class CategoryViewModel: NamedEntity, IOrderedEntity
+    {
+        public CategoryViewModel()
+        {
+            ChildCategories = new List<CategoryViewModel>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+               
+        public List<CategoryViewModel> ChildCategories { get; set; }
+
+        public CategoryViewModel ParentCategory { get; set; }
+    }
+}
