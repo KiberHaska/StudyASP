0c5e14091898d8253ac3cdcb0c219515ea141312
Astrid <email@email.local>
2020-11-11 15:20:20 +0300

    DZ4: Add ViewComponent for Brands

---
+++ ASPlevel1/ViewComponents/BrandsViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using ASPlevel1.Infrastructure.Interfaces;
+using ASPlevel1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPlevel1.ViewComponents
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public BrandsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var brands = GetBrands();
+            return View(brands);
+        }
+
+        private IEnumerable<BrandViewModel> GetBrands()
+        {
+            var listBrands = _productService.GetBrands();
+            return listBrands.Select(b => new BrandViewModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+                Order = b.Order,
+                ProductsCount = 0
+            }).OrderBy(b => b.Order).ToList();
+        }
+
+    }
+ }
+
