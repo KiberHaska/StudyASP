56e142df9a950f6fd2a74c066e3af7773d2236c3
Kim <Kim@dev>
2020-11-13 15:10:44 +0300

    Revert "Lesson5: bug fixed"
    This reverts commit 03dc6241596e24438da93efcb8fac7c0280277a8.

--- AspLevel1.Domain/Entities/Base/Interfaces/INamedEntity.cs
+++ AspLevel1.Domain/Entities/Base/Interfaces/INamedEntity.cs
@@ -4,7 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace AspLevel1.Domain.Entities.Base.Interfaces
 {
     public interface INamedEntity : IBaseEntity
-    {        
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int Id { get; set; }
         string Name { get; set; }
     }
--- AspLevel1.Domain/Entities/Base/NamedEntity.cs
+++ AspLevel1.Domain/Entities/Base/NamedEntity.cs
@@ -1,13 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using AspLevel1.Domain.Entities.Base.Interfaces;
+﻿using AspLevel1.Domain.Entities.Base.Interfaces;
 
 namespace AspLevel1.Domain.Entities.Base
 {
     public class NamedEntity : INamedEntity
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
     }
