ba49dc6dadde37bac0c31948a3287a5859d23b55
Kim <Kim@dev>
2020-12-07 18:06:21 +0300

    Lesson5: Init BD start data ( false )

--- ASPlevel1/Program.cs
+++ ASPlevel1/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPlevel1.DAL;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,25 @@ namespace ASPlevel1
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()) // нужно для получения DbContext
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    ASPlevel1Context context = services.GetRequiredService<ASPlevel1Context>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Oops. Something went wrong at DB initializing...");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
