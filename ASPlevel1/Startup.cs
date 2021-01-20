5887325c4fc7d0ff319594662e007efbd5da9964
Kim <Kim@dev>
2021-01-20 17:54:10 +0300

    Lesson8: Add Area admin

--- ASPlevel1/Startup.cs
+++ ASPlevel1/Startup.cs
@@ -108,6 +108,10 @@ namespace ASPlevel1
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "area",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 
