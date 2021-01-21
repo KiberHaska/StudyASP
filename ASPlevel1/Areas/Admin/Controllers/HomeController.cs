a886c37afb5085c3e39acf3bcacfc14ff79159b2
Kim <Kim@dev>
2021-01-21 18:04:28 +0300

    Lesson8: Admin area, add View

--- ASPlevel1/Areas/Admin/Controllers/HomeController.cs
+++ ASPlevel1/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspLevel1.Domain.Entities;
+using ASPlevel1.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,18 @@ using System.Threading.Tasks;
 
 namespace ASPlevel1.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles ="Admins")]
     public class HomeController : Controller
     {
+        private readonly IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_productService.GetProducts(new ProductFilter()));
         }
     }
 }
