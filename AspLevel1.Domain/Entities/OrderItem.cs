d7744bed36973f021bab93e6a647ed86a0174344
Kim <Kim@dev>
2021-01-22 17:28:05 +0300

    Lesson8: Add Order

---
+++ AspLevel1.Domain/Entities/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using AspLevel1.Domain.Entities.Base;
+
+namespace AspLevel1.Domain.Entities
+{
+    public class OrderItem : BaseEntity
+    {
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Order Order { get; set; } // сгенерирует внешний ключ в БД
+        public virtual Product Product { get; set; } // сгенерирует внешний ключ в БД
+    }
+   
+}
